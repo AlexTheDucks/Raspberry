56fbef5365bfaea9d651efe37d607376aa12ae40
Yannick <yannick.schuitemaker@stud.hslu.ch>
2020-10-31 16:13:48 +0100

    Test Ducks

---
+++ SchaereSteiPapier/SSP.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchaereSteiPapier
+{
+    enum SSP
+    {
+        Schaere = 0,
+        Stein = 1,
+        Papier = 2
+    }
+}
