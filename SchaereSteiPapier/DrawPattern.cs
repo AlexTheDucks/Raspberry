c434ed537985b176f7404246a8c77a47ec57f7cb
Yannick <yannick.schuitemaker@stud.hslu.ch>
2020-11-01 14:06:16 +0100

    Draw Pattern

---
+++ SchaereSteiPapier/DrawPattern.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using Explorer700Library;
+
+namespace SchaereSteiPapier
+{
+    static class DrawPattern
+    {
+
+        public static void drawScore(Graphics g, int playerPoint, int opPoint)
+        {
+            g.DrawString("You: " + playerPoint, new Font(new FontFamily("arial"), 8, FontStyle.Bold), Brushes.Blue, new PointF(5, 50));
+            g.DrawString("Op.: " + opPoint, new Font(new FontFamily("arial"), 8, FontStyle.Bold), Brushes.Blue, new PointF(90, 50));
+
+        }
+
+        public static void drawTimer(Graphics g, int TimeLeft)
+        {
+            if (TimeLeft != -1)
+            {
+                g.DrawString(TimeLeft.ToString(), new Font(new FontFamily("arial"), 8, FontStyle.Bold), Brushes.Blue, new PointF(61, 50));
+            }
+        }
+
+        public static void drawAttackCoose(Graphics g)
+        {
+            Pen pen = new Pen(Brushes.Blue);
+            Brush brush = Brushes.Blue;
+
+            PointF[] pointsLeft = new PointF[3];
+            pointsLeft[0] = new PointF(30, 20);
+            pointsLeft[1] = new PointF(30, 30);
+            pointsLeft[2] = new PointF(25, 25);
+
+            PointF[] pointsRight = new PointF[3];
+            pointsRight[0] = new PointF(98, 20);
+            pointsRight[1] = new PointF(98, 30);
+            pointsRight[2] = new PointF(103, 25);
+
+            g.DrawRectangle(pen, new Rectangle(39, 0, 50, 50));
+            g.FillPolygon(brush, pointsLeft);
+            g.FillPolygon(brush, pointsRight);
+        }
+
+
+    }
+
+}
+}
--- SchaereSteiPapier/DrawingMethods.cs
+++ SchaereSteiPapier/DrawingMethods.cs
@@ -8,37 +8,22 @@ namespace SchaereSteiPapier
 {
     static class DrawingMethods
     {
-        public static Graphics drawStateAttack(Explorer700 board, int playerPoint, int opPoint, int Timeleft)
+        public static Graphics drawStateAttack(Explorer700 board, int playerPoint, int opPoint, int TimeLeft)
         {
             Graphics g = board.Display.Graphics;
+
             g.Clear(default);
 
-            Pen pen = new Pen(Brushes.Blue);
-            Brush brush = Brushes.Blue;
-            PointF[] pointsLeft = new PointF[3];
-            pointsLeft[0] = new PointF(30, 20);
-            pointsLeft[1] = new PointF(30, 30);
-            pointsLeft[2] = new PointF(25, 25);
+            DrawPattern.drawAttackCoose(g);
+            DrawPattern.drawTimer(g, TimeLeft);
+            DrawPattern.drawScore(g, playerPoint, playerPoint);
 
-            PointF[] pointsRight = new PointF[3];
-            pointsRight[0] = new PointF(98, 20);
-            pointsRight[1] = new PointF(98, 30);
-            pointsRight[2] = new PointF(103, 25);
-
-            g.DrawRectangle(pen, new Rectangle(39, 0, 50, 50));
-            g.FillPolygon(brush, pointsLeft);
-            g.FillPolygon(brush, pointsRight);
-            if (Timeleft != -1)
-            {
-                g.DrawString(Timeleft.ToString(), new Font(new FontFamily("arial"), 8, FontStyle.Bold), Brushes.Blue, new PointF(61, 50));
-            }
-            g.DrawString("You: " + playerPoint, new Font(new FontFamily("arial"), 8, FontStyle.Bold), Brushes.Blue, new PointF(5, 50));
-            g.DrawString("Op.: " + opPoint, new Font(new FontFamily("arial"), 8, FontStyle.Bold), Brushes.Blue, new PointF(90, 50));
             board.Display.Update();
             return g;
         }
 
 
-     
+
     }
+      
 }
